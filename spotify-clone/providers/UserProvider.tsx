a2d653489e130babacf033c0db88b5df2fe08bbf
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-06-05 16:14:35 +0300

    providers for auth and supabase

---
+++ spotify-clone/providers/UserProvider.tsx
@@ -0,0 +1,11 @@
+"use client ";
+import { MyUserContextProvider } from "@/hooks/useUser";
+import React from "react";
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  return <MyUserContextProvider>{children}</MyUserContextProvider>;
+};
+
+export default UserProvider;
