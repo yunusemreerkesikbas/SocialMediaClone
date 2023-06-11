4ed6fde8247a2bff590cc61afd4ae7d7f1094c80
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-06-12 02:26:20 +0300

    upload modal and functionality

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -2,9 +2,18 @@
 import React from "react";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
   const onClick = () => {
-    //
+    if (!user) {
+      return authModal.onOpen();
+    }
+    return uploadModal.onOpen();
   };
 
   return (
