37ae76aafd08ea79b78719f4ca8e8e49050c3e14
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-06-13 18:21:50 +0300

    added fovorites functionality

--- spotify-clone/app/liked/components/LikedContent.tsx
+++ spotify-clone/app/liked/components/LikedContent.tsx
@@ -1,23 +1,31 @@
 "use client";
-
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-interface SearchContentProps {
+interface LikedContentProps {
   songs: Song[];
 }
-
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No songs found
+        No liked songs
       </div>
     );
   }
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6 ">
+    <div className="flex flex-col gap-y-2 w-full p-6">
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
@@ -30,4 +38,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
