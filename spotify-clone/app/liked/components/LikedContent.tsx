42e8acb9445dfdbabf752329f46823edb0296a89
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-06-14 02:16:16 +0300

    added player functionality

--- spotify-clone/app/liked/components/LikedContent.tsx
+++ spotify-clone/app/liked/components/LikedContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
 import { useRouter } from "next/navigation";
@@ -10,6 +11,7 @@ interface LikedContentProps {
   songs: Song[];
 }
 const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
   const router = useRouter();
   const { isLoading, user } = useUser();
   useEffect(() => {
@@ -29,7 +31,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
