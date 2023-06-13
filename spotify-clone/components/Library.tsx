caf76b56812c2e0935c05a9ec1e5df1719e24f76
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-06-13 17:34:04 +0300

    added songs fetching and list display

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -5,7 +5,12 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
-const Library = () => {
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
+interface LibraryProps {
+  songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -21,7 +26,7 @@ const Library = () => {
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist size={26} className="text-neutral-400 " />
-          <p className="text-neutral-400 font-medium text-md ">Kitaplık</p>
+          <p className="text-neutral-400 font-medium text-md ">Library</p>
         </div>
         <AiOutlinePlus
           onClick={onClick}
@@ -29,7 +34,11 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">Şarkı Listesi </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
--- spotify-clone/next.config.js
+++ spotify-clone/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "bpxbnpgccmzrkrwlbbwe.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
